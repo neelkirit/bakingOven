faab88d87c946cab92e267fffc4019948c179e67
neelkirit <neelkirit2@gmail.com>
2019-10-27 18:00:13 -0400

    Refactored
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    setup_requires=["pbr"],
-    pbr=True
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
 )
