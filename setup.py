0c0c53731e3c71bb6f03131fa0ef26531fccfe2e
neelkirit <neelkirit2@gmail.com>
2019-10-27 17:15:15 -0400

    Dockerized this motherfiucker
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-    name='flaskr',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'flask',
-    ],
+    setup_requires=["pbr"],
+    pbr=True
 )
