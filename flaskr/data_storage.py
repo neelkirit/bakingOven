e4692843aaf5bbd1be2af79a6f2090484eeb3854
neelkirit <neelkirit2@gmail.com>
2019-11-03 21:07:27 -0500

    Storing to influxdb
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- flaskr/data_storage.py
+++ flaskr/data_storage.py
@@ -9,8 +9,11 @@ bp = Blueprint('data_storage', __name__)
 @bp.route('/store', methods=["GET", "POST"])
 def store():
     if request.method == 'POST':
-        url = 'http://localhost:8086/write?db=mydb'
-        payload = 'cpu_load_short,host=server01,region=us-west value=0.690'
+        angle = request.json['angle']
+        user = request.json['user']
+        product = request.json['product']
+        url = 'http://localhost:8086/write?db=ineck'
+        payload = 'angle,user=' + user + ',product=' + product + ' value=' + angle
         headers = {}
         res = requests.post(url, data=payload, headers=headers)
         print(res)
