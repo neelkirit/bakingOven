2d018f9c227ba6ff3829f8295598eb7bee0e50fc
neelkirit <neelkirit2@gmail.com>
2020-03-02 21:35:07 -0500

    Fixed SQL
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,7 +1,7 @@
 --DROP TABLE IF EXISTS User;
 
 CREATE TABLE User (
-  id int PRIMARY KEY AUTOINCREMENT,
+  id int PRIMARY KEY AUTO_INCREMENT,
   name varchar(255) NOT NULL,
   age int NOT NULL,
   profile_picture varchar(255) NOT NULL,
